fcafa20aa63056a0903ae93b41194d88a3f9c05e
Joshua Shaffer <joshua@joshuatshaffer.com>
2022-01-22 12:09:02 -0600

    Cleaned.

--- lib/html-template-tag.spec.ts
+++ lib/html-template-tag.spec.ts
@@ -1,18 +1,10 @@
 import { html } from "./html-template-tag";
-import { css } from "./css-template-tag";
 
 describe("foo", () => {
   it("works", () => {
     console.log(
       html`<html>
         <head>
-          <style>
-            ${html.raw(css`
-              body {
-                background-color: aliceblue;
-              }
-            `)}
-          </style>
           <style>
             body {
               background-color: aliceblue;

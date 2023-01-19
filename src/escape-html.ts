b36fe1844c706b6cd28c0698aa7b82fc4e070cb0
Joshua Shaffer <joshua@joshuatshaffer.com>
2023-01-19 08:50:04 -0600

    Add notes on HTML escaping.

--- src/escape-html.ts
+++ src/escape-html.ts
@@ -1,4 +1,17 @@
-/** Replace potentially troublesome characters with their HTML character entities. */
+/**
+ * Replace potentially troublesome characters (`&`, `<`, `>`, `'`, `"`, and `` `
+ * ``) with their HTML character entities.
+ *
+ * Note: Always quote attribute values. Escaping works fine in attribute values,
+ * but it will not stop attribute injection if the attribute is unquoted. For
+ * example, ``html`<div foo=${'bar style=color:red}>test</div>`` produces `<div
+ * foo=bar style=color:red>test</div>`. Quoting prevents this; ``html`<div
+ * foo="${'bar" style="color:red}">test</div>`` produces `<div foo="bar&quot;
+ * style=&quot;color:red">test</div>`.
+ *
+ * Note: Escaping does not work for the content of `<script>` and `<style>`
+ * tags. The content of these tags does not use HTML character references.
+ */
 export function escapeHtml(unescaped: string): string {
   return unescaped.replace(
     /[&<>"'`]/g,

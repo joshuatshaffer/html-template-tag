23ef6ccabcd361f502edb8c8f51dcb68d377fb46
Joshua Shaffer <joshua@joshuatshaffer.com>
2023-01-19 09:24:28 -0600

    Move helpers back to namespace.

--- src/html-template-tag.ts
+++ src/html-template-tag.ts
@@ -17,3 +17,22 @@ export function html(
 
   return new HtmlFragment(output);
 }
+
+export namespace html {
+  /** Like {@link Array.join}, but with {@link HtmlFragment}s instead of strings. */
+  export function join(
+    array: readonly HtmlInterpolation[],
+    separator: HtmlInterpolation = ""
+  ) {
+    return new HtmlFragment(
+      array
+        .map(handleHtmlInterpolation)
+        .join(handleHtmlInterpolation(separator))
+    );
+  }
+
+  /** Convert a string to an {@link HtmlFragment} without escaping. */
+  export function raw(content: string) {
+    return new HtmlFragment(content);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from "./escape-html";
-export * from "./helpers";
 export * from "./html-fragment";
 export * from "./html-interpolation";
 export * from "./html-template-tag";

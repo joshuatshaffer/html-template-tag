58b3e7324acf395501ff45f0ab684fd1d1be2a1b
Joshua Shaffer <joshua@joshuatshaffer.com>
2023-01-19 12:20:17 -0600

    Remove namespace because it caused problems with go to definition.

--- src/html-template-tag.ts
+++ src/html-template-tag.ts
@@ -18,51 +18,45 @@ export function html(
   return new HtmlFragment(output);
 }
 
-export namespace html {
-  /** Like {@link Array.join}, but with {@link HtmlFragment}s instead of strings. */
-  export function join(
-    array: readonly HtmlInterpolation[],
-    separator: HtmlInterpolation = ""
-  ) {
-    return new HtmlFragment(
-      array
-        .map(handleHtmlInterpolation)
-        .join(handleHtmlInterpolation(separator))
-    );
-  }
+/** Like {@link Array.join}, but with {@link HtmlFragment}s instead of strings. */
+export function joinHtml(
+  array: readonly HtmlInterpolation[],
+  separator: HtmlInterpolation = ""
+) {
+  return new HtmlFragment(
+    array.map(handleHtmlInterpolation).join(handleHtmlInterpolation(separator))
+  );
+}
 
-  /** Convert a string to an {@link HtmlFragment} without escaping. */
-  export function raw(content: string) {
-    return new HtmlFragment(content);
-  }
+/** Convert a string to an {@link HtmlFragment} without escaping. */
+export function rawHtml(content: string) {
+  return new HtmlFragment(content);
+}
 
-  /**
-   * Convert an object to HTML attributes.
-   */
-  export function attrs(attributes: Record<string, HtmlInterpolation>) {
-    // TODO: Guard against invalid attribute names.
-    return html.join(
-      Object.entries(attributes).map(
-        ([name, value]) => html`${name}="${value}"`
-      ),
-      " "
-    );
-  }
+/**
+ * Convert an object to HTML attributes.
+ */
+export function htmlAttrs(attributes: Record<string, HtmlInterpolation>) {
+  // TODO: Guard against invalid attribute names.
+  return joinHtml(
+    Object.entries(attributes).map(([name, value]) => html`${name}="${value}"`),
+    " "
+  );
+}
 
-  /**
-   * If content is `null` or `undefined`, render a self closing tag. To render
-   * an empty element, pass empty string to content.
-   */
-  export function tag(
-    name: string,
-    attributes?: Record<string, HtmlInterpolation>,
-    content?: HtmlInterpolation
-  ) {
-    // TODO: Guard against invalid tag names.
-    return html`<${name}${
-      attributes && Object.keys(attributes).length > 0
-        ? html` ${html.attrs(attributes)}`
-        : null
-    }${content != null ? html`>${content}</${name}>` : html` />`}`;
-  }
+/**
+ * If content is `null` or `undefined`, render a self closing tag. To render
+ * an empty element, pass empty string to content.
+ */
+export function htmlTag(
+  name: string,
+  attributes?: Record<string, HtmlInterpolation>,
+  content?: HtmlInterpolation
+) {
+  // TODO: Guard against invalid tag names.
+  return html`<${name}${
+    attributes && Object.keys(attributes).length > 0
+      ? html` ${htmlAttrs(attributes)}`
+      : null
+  }${content != null ? html`>${content}</${name}>` : html` />`}`;
 }
